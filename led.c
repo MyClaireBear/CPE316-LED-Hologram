269d11ee2e4838496aea9bc26bda1ae5a0409685
Claire Chen <lairecay01@gmail.com>
2021-11-30 14:12:40 -0800

    Add files via upload

---
+++ led.c
@@ -0,0 +1,53 @@
+/*
+ * led.c
+ *
+ *  Created on: Nov 30, 2021
+ *      Author: Claire
+ */
+
+#include "stm32l4xx_hal.h"
+
+void LED_init(void) {
+	/*****Init PA0 - PA4 for LED output*****/
+	// Start clock for GPIOA
+	RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
+
+	// set to output mode
+	GPIOA->MODER &= ~(GPIO_MODER_MODE0 |
+						GPIO_MODER_MODE1 |
+						GPIO_MODER_MODE2 |
+						GPIO_MODER_MODE3 |
+						GPIO_MODER_MODE4);
+
+	GPIOA->MODER |= ~(GPIO_MODER_MODE0_0 |
+						GPIO_MODER_MODE1_0 |
+						GPIO_MODER_MODE2_0 |
+						GPIO_MODER_MODE3_0 |
+						GPIO_MODER_MODE4_0);
+
+	// set to push pull
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT0 |
+						GPIO_OTYPER_OT1 |
+						GPIO_OTYPER_OT2 |
+						GPIO_OTYPER_OT3 |
+						GPIO_OTYPER_OT4);
+
+	// pull-down
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD0 |
+						GPIO_PUPDR_PUPD1 |
+						GPIO_PUPDR_PUPD2 |
+						GPIO_PUPDR_PUPD3 |
+						GPIO_PUPDR_PUPD4);
+
+	// very high speed
+	GPIOA->OSPEEDR |=  ((3 << GPIO_OSPEEDR_OSPEED0_Pos) |
+						(3 << GPIO_OSPEEDR_OSPEED1_Pos) |
+						(3 << GPIO_OSPEEDR_OSPEED2_Pos) |
+						(3 << GPIO_OSPEEDR_OSPEED3_Pos) |
+						(3 << GPIO_OSPEEDR_OSPEED4_Pos));
+}
+
+
+
+
+
---
+++ led.h
@@ -0,0 +1,13 @@
+/*
+ * led.h
+ *
+ *  Created on: Nov 30, 2021
+ *      Author: Claire
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+
+void LED_init(void);
+
+#endif /* INC_LED_H_ */
