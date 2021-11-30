ed264ab6dac0d1babf690beff63b599ba038e55c
NicholasArner <33156299+NicholasArner@users.noreply.github.com>
2021-11-29 21:46:37 -0800

    changed moder to match ref manual
    also changed a few other register values to match  what is written in the reference manual

--- led.c
+++ led.c
@@ -6,39 +6,49 @@
  */
 
 #include "stm32g031xx.h"
+#include "main.h"
 
 void LED_init(void) {
 	// Init PA0 - PA4 for LED output
 	// Start clock for GPIOA
-	RCC->IOPENR   |=  (RCC_IOPENR_GPIOAEN);
+	RCC->IOPENR   |=  (RCC_IOPENR_GPIOBEN);
 
 	// set to output mode
-	GPIOA->MODER   &= ~(GPIO_MODER_MODE0 |
+	GPIOB->MODER   &= ~(GPIO_MODER_MODE0 |
 						GPIO_MODER_MODE1 |
 						GPIO_MODER_MODE2 |
 						GPIO_MODER_MODE3 |
 						GPIO_MODER_MODE4);
 
+	GPIOB->MODER   |= (GPIO_MODER_MODE0_0 |
+							GPIO_MODER_MODE1_0 |
+							GPIO_MODER_MODE2_0 |
+							GPIO_MODER_MODE3_0 |
+							GPIO_MODER_MODE4_0);
+
+
+
 	// set to push pull
-	GPIOA->OTYPER  &= ~(GPIO_OTYPER_OT0 |
+	GPIOB->OTYPER  &= ~(GPIO_OTYPER_OT0 |
 						GPIO_OTYPER_OT1 |
 						GPIO_OTYPER_OT2 |
 						GPIO_OTYPER_OT3 |
 						GPIO_OTYPER_OT4);
 
-	// pull-down
-	GPIOA->PUPDR   &= ~(GPIO_PUPDR_PUPD0 |
+	// no pull-up or pull-down
+	GPIOB->PUPDR   &= ~(GPIO_PUPDR_PUPD0 |
 						GPIO_PUPDR_PUPD1 |
 						GPIO_PUPDR_PUPD2 |
 						GPIO_PUPDR_PUPD3 |
 						GPIO_PUPDR_PUPD4);
 
-	// very high speed
-	GPIOA->OSPEEDR |=  ((3 << GPIO_OSPEEDR_OSPEED0_Pos) |
-						(3 << GPIO_OSPEEDR_OSPEED1_Pos) |
-						(3 << GPIO_OSPEEDR_OSPEED2_Pos) |
-						(3 << GPIO_OSPEEDR_OSPEED3_Pos) |
-						(3 << GPIO_OSPEEDR_OSPEED4_Pos));
-}
+	// very low speed
+	GPIOB->OSPEEDR &=  ~((GPIO_OSPEEDR_OSPEED0)|
+						(GPIO_OSPEEDR_OSPEED1) |
+						(GPIO_OSPEEDR_OSPEED2) |
+						(GPIO_OSPEEDR_OSPEED3) |
+						(GPIO_OSPEEDR_OSPEED4));
 
+
+}
 
